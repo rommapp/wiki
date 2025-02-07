f6cb85f62e1d5676d23f2b8da377e74c6aa2396f
Georges-Antoine Assi <contact@me.gantoine.com>
2025-02-06 19:47:24 -0500

    add justfile

---
+++ push_wiki.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the /romm.wiki directory
+cd "$(dirname "$0")/romm.wiki" || exit 1
+
+# Add all files to git
+git add .
+
+# Commit changes with a timestamped message
+git commit -m "Auto-commit: $(date '+%Y-%m-%d %H:%M:%S')"
+
+# Push to the master branch
+# git push origin master
